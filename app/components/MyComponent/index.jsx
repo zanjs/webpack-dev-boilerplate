604c2bd6a99b558b40861ebb1c73c1bde5e1d83c
zanjs <root@zanjs.com>
2016-05-19 18:34:34 +0800

    :cat: init

---
+++ app/components/MyComponent/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <h1 className="demo">
+        <span className="glyphicon glyphicon-th"></span>
+        一起来维护这个项目
+        <span className="glyphicon glyphicon-plane"></span>
+      </h1>
+    );
+  }
+}
+
+export default MyComponent;
